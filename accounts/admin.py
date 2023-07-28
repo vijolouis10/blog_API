fdabe6a6b36f6bf7eb386634b63e24c4e3e4ca62
VIJO LOUIS <vijolouis11@gmail.com>
2023-07-28 13:43:00 +0530

    Permissions added

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,15 +8,9 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = [
-        "email",
-        "username",
-        "name",
-        "is_staff",
-]
-
-fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
-add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name",)}),)
+    list_display = ["email","username","name","is_staff"]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name",)}),)
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
+from . permissions import IsAuthorOrReadOnly
 from .models import Post
 from . serializers import PostSerializer
 
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
+    permission_classes=(IsAuthorOrReadOnly, )
     queryset=Post.objects.all()
     serializer_class=PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes=(IsAuthorOrReadOnly, )
     queryset=Post.objects.all()
     serializer_class=PostSerializer 
